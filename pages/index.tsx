48e54f4d12b9028edfe726294198304d31430add
Biruk Kebede <biruk.key@gmail.com>
2022-04-16 23:58:21 +0100

    Corrected import statement due to deployment issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import prisma from '../lib/prisma.ts'
+import prisma from '../lib/prisma'
 import { GetStaticProps } from 'next'
 import Home from './home'
 
