8354b257eb7c33b6585c08ed7b985465cc496a6c
Biruk Kebede <biruk.key@gmail.com>
2022-12-28 15:11:00 +0000

    fixed type error with dynamic path /details

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import {useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import '../styles/globals.css'
 import Link from 'next/link'
 import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
-	const [project, setProject] = useState([])
+	const [project, setProject] = useState({})
 	return (
 		<Layout>
 			<Head>
--- pages/details.js
+++ pages/details.js
@@ -10,8 +10,11 @@ import styles from '../styles/details.module.css'
 
 const ProjectDetail = ({ project }) => {
 	const [showMore, setShowMore] = useState(false)
-	const descriptionLines = project.description.split(/\n/)
-	const lessText = descriptionLines.join(' ').slice(0, 300)
+	let descriptionLines, lessText
+	if (project.description) {
+		descriptionLines = project.description.split(/\n/)
+		lessText = descriptionLines.join(' ').slice(0, 300)
+	}
 
 	const toggleText = () => {
 		setShowMore(!showMore)
@@ -25,61 +28,68 @@ const ProjectDetail = ({ project }) => {
 			animate={{ opacity: 1 }}
 			exit={{ opacity: 0.5 }}
 		>
-			<div className={styles['project-wrapper']}>
-				<h4 className={styles['project-title']}>{project.title}</h4>
-				<img src={`/images/${project.image}`} alt='' />
-				<div className={styles['read-more-read-less-wrapper']}>
-					{descriptionLines.join(' ').length > lessText.length ? (
-						<div key='read-more' className={styles.description}>
-							{!showMore
-								? lessText
-								: descriptionLines.map((line, idx) => (
-										<div key={idx}>
-											<p className={styles.description}>{line}</p>
-										</div>
-								  ))}
-							<span key='span' onClick={toggleText}>
-								{!showMore ? '...read more ▼' : 'read less ▲'}
-							</span>
-						</div>
-					) : (
-						descriptionLines.map((line, idx) => (
-							<div key={idx}>
-								<p className={styles.description}>{line}</p>
+			{descriptionLines && (
+				<div className={styles['project-wrapper']}>
+					<h4 className={styles['project-title']}>{project.title}</h4>
+					<img src={`/images/${project.image}`} alt='' />
+					<div className={styles['read-more-read-less-wrapper']}>
+						{descriptionLines &&
+						descriptionLines.join(' ').length > lessText.length ? (
+							<div key='read-more' className={styles.description}>
+								{!showMore
+									? lessText
+									: descriptionLines.map((line, idx) => (
+											<div key={idx}>
+												<p className={styles.description}>{line}</p>
+											</div>
+									  ))}
+								<span key='span' onClick={toggleText}>
+									{!showMore ? '...read more ▼' : 'read less ▲'}
+								</span>
 							</div>
-						))
-					)}
+						) : (
+							descriptionLines &&
+							descriptionLines.map((line, idx) => (
+								<div key={idx}>
+									<p className={styles.description}>{line}</p>
+								</div>
+							))
+						)}
+					</div>
+					<div className={styles['project-links-wrapper']}>
+						<motion.a
+							href={project.repository}
+							target='_blank'
+							rel='noreferrer'
+							className={styles['github-link']}
+							whileHover={{
+								scale: 1.1,
+								backgroundColor: 'var(--button-hover-bg)',
+							}}
+							whileTap={{ scale: 0.9 }}
+						>
+							<FontAwesomeIcon
+								icon={faGithub}
+								className={styles['fa-github']}
+							/>
+							&nbsp; GitHub
+						</motion.a>
+						<motion.a
+							href={project.url}
+							target='_blank'
+							rel='noreferrer'
+							className={styles['live-demo-link']}
+							whileHover={{
+								scale: 1.1,
+								backgroundColor: 'var(--button-hover-bg)',
+							}}
+							whileTap={{ scale: 0.9 }}
+						>
+							Live Demo
+						</motion.a>
+					</div>
 				</div>
-				<div className={styles['project-links-wrapper']}>
-					<motion.a
-						href={project.repository}
-						target='_blank'
-						rel='noreferrer'
-						className={styles['github-link']}
-						whileHover={{
-							scale: 1.1,
-							backgroundColor: 'var(--button-hover-bg)',
-						}}
-						whileTap={{ scale: 0.9 }}
-					>
-						<FontAwesomeIcon icon={faGithub} className={styles['fa-github']} />
-						&nbsp; GitHub
-					</motion.a>
-					<motion.a
-						href={project.url}
-						target='_blank'
-						rel='noreferrer'
-						className={styles['live-demo-link']}
-						whileHover={{
-							scale: 1.1,
-							backgroundColor: 'var(--button-hover-bg)',
-						}}
-						whileTap={{ scale: 0.9 }}
-					>
-						Live Demo
-					</motion.a>
-				</div>
-			</div>
+			)}
 		</motion.main>
 	)
 }
