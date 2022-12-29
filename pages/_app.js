8f0de675a343f536e1af19e90c990d51989c83d2
Biruk Kebede <biruk.key@gmail.com>
2022-12-29 07:01:58 +0000

    removed a link

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,6 @@ function MyApp({ Component, pageProps }) {
 		<Layout>
 			<Head>
 				<title>Biruk | Full Stack Web Developer</title>
-				<style>
-					@import
-					url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Montserrat:wght@100;200;300;400;500;600;700;800&family=Playfair+Display:wght@400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600&family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap');
-				</style>
 			</Head>
 			<Component {...pageProps} project={project} setProject={setProject} />
 		</Layout>
