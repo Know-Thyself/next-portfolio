321c47e6ea8857c5aab18e103e24f2905c390a14
Biruk Kebede <biruk.key@gmail.com>
2023-09-27 05:11:08 +0100

    style: custom responsive toggler button

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,96 +19,93 @@ export const getStaticProps: GetStaticProps = async () => {
 	return { props: { data } }
 }
 
-const HomePage = ({ data }) => {
-	const id = data.map((portfolio) => portfolio.id).pop()
-	const about = data.map((portfolio) => portfolio.content.about)
+function HomePage({ data }): JSX.Element {
+    const id = data.map((portfolio: { id: any} ) => portfolio.id).pop()
+    const about = data.map((portfolio: { content: { about: any} } ) => portfolio.content.about)
 
-	const str = about[0].intro
-	const image = about[0].image
-	const lines = str.split(/\n/)
-	const withBreaks = lines.flatMap((line, index) =>
-		index > 0
-			? [<br key={`br-${index}`} />, <Fragment key={index}>{line}</Fragment>]
-			: [line]
-	)
-	const variants = {
-		start: {
-			opacity: 0,
-		},
-		end: {
-			opacity: 1,
-			transition: {
-				type: 'spring',
-				stiffness: 60,
-				duration: 0.5,
-				delay: 0.5,
-			},
-		},
-		exit: {
-			opacity: 0,
-		},
-	}
+    const str = about[0].intro
+    const image = about[0].image
+    const lines = str.split(/\n/)
+    const withBreaks = lines.flatMap((line, index) => index > 0
+        ? [<br key={`br-${index}`} />, <Fragment key={index}>{line}</Fragment>]
+        : [line]
+    )
+    const variants = {
+        start: {
+            opacity: 0,
+        },
+        end: {
+            opacity: 1,
+            transition: {
+                type: 'spring',
+                stiffness: 60,
+                duration: 0.5,
+                delay: 0.5,
+            },
+        },
+        exit: {
+            opacity: 0,
+        },
+    }
 
-	return (
-		<AnimatePresence>
-			<Head>
-				<title>Biruk | Full-Stack Developer</title>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<motion.div
-				className={styles.home}
-				variants={variants}
-				initial='start'
-				animate='end'
-				exit='exit'
-			>
-				<section className={styles.hero}>
-					<div className={styles['hero-text-container']}>
-						<h1 className={styles['primary-text']}>Hello & Welcome!</h1>
-						<Image
-							className={styles['profile-photo']}
-							src={`/images/${image}`}
-							priority={true}
-							loading='eager'
-							width={200}
-							height={200}
-							alt='Author'
-						/>
-						<h3>Thank you for visiting my portfolio website!</h3>
-						<h4 className={styles.intro}>{withBreaks}</h4>
-						<div className={styles['links-wrapper']}>
-							<motion.a
-								href='https://github.com/Know-Thyself'
-								target='_blank'
-								rel='noreferrer'
-								className={styles.link}
-								whileHover={{
-									scale: 1.2,
-								}}
-								whileTap={{ scale: 0.8 }}
-							>
-								<FontAwesomeIcon
-									icon={faGithub}
-									className={styles['fa-github']}
-								/>
-								&nbsp; GitHub
-							</motion.a>
-							<motion.div
-								whileHover={{
-									scale: 1.2,
-								}}
-								whileTap={{ scale: 0.8 }}
-							>
-								<Link href='/projects' className={styles.link}>
-									Projects
-								</Link>
-							</motion.div>
-						</div>
-					</div>
-				</section>
-			</motion.div>
-		</AnimatePresence>
-	)
+    return (
+        <AnimatePresence>
+            <Head>
+                <title>Biruk | Full-Stack Developer</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <motion.div
+                className={styles.home}
+                variants={variants}
+                initial='start'
+                animate='end'
+                exit='exit'
+            >
+                <section className={styles.hero}>
+                    <div className={styles['hero-text-container']}>
+                        <h1 className={styles['primary-text']}>Hello & Welcome!</h1>
+                        <Image
+                            className={styles['profile-photo']}
+                            src={`/images/${image}`}
+                            priority={true}
+                            loading='eager'
+                            width={200}
+                            height={200}
+                            alt='Author' />
+                        <h3>Thank you for visiting my portfolio website!</h3>
+                        <h4 className={styles.intro}>{withBreaks}</h4>
+                        <div className={styles['links-wrapper']}>
+                            <motion.a
+                                href='https://github.com/Know-Thyself'
+                                target='_blank'
+                                rel='noreferrer'
+                                className={styles.link}
+                                whileHover={{
+                                    scale: 1.2,
+                                }}
+                                whileTap={{ scale: 0.8 }}
+                            >
+                                <FontAwesomeIcon
+                                    icon={faGithub}
+                                    className={styles['fa-github']} />
+                                &nbsp; GitHub
+                            </motion.a>
+                            <motion.div
+                                whileHover={{
+                                    scale: 1.2,
+                                }}
+                                whileTap={{ scale: 0.8 }}
+                            >
+                                <Link href='/projects' className={styles.link}>
+                                    Projects
+                                </Link>
+                            </motion.div>
+                        </div>
+                    </div>
+                </section>
+            </motion.div>
+        </AnimatePresence>
+    )
 }
 
 export default HomePage
