fe7d444aa1d5015b521e759d4bdd88ca5d09788e
Biruk Kebede <biruk.key@gmail.com>
2023-09-30 00:28:54 +0100

    experimenting with colors

--- pages/contact.js
+++ pages/contact.js
@@ -300,7 +300,7 @@ const Contact = () => {
 										'& .MuiInputBase-root': {
 											color: 'var(--primary-text-color)',
 											borderBottom: '1px solid var(--primary-text-color)',
-											backgroundColor: 'var(--tertiary-bg)',
+											backgroundColor: 'var(--secondary-bg)',
 										},
 									}}
 								/>
