ece735372594315d450e5e8a2630e60897e04749
Biruk Kebede <biruk.key@gmail.com>
2023-10-04 20:13:30 +0100

    prisma generate for production build

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Script from 'next/script'
 import 'bootswatch/dist/slate/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }) {
-  const [project, setProject] = useState({})
+	const [project, setProject] = useState({})
 	return (
 		<Layout>
 			<Head>
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
 			<Script
 				src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'
 				integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4'
-				crossorigin='anonymous'
+				crossOrigin='anonymous'
 			/>
 			<Component {...pageProps} project={project} setProject={setProject} />
 		</Layout>
