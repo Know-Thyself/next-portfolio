5ff4c8474871cb40b516c5d11cc53d309a0bf6cd
Biruk Kebede <biruk.key@gmail.com>
2023-10-07 05:04:40 +0100

    adding one more project

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/contact.module.css'
 import Box from '@mui/material/Box'
