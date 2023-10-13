143528857c5ea2f8b0f612f4021a3f8c8305911c
Biruk Kebede <biruk.key@gmail.com>
2023-10-13 13:38:22 +0100

    dark mode color scheme

--- pages/contact.js
+++ pages/contact.js
@@ -300,7 +300,7 @@ const Contact = () => {
 										'& .MuiInputBase-root': {
 											color: 'var(--primary-text-color)',
 											borderBottom: '1px solid var(--primary-text-color)',
-											backgroundColor: 'var(--complementary-bg)',
+											backgroundColor: 'var(--tertiary-bg)',
 										},
 									}}
 								/>
